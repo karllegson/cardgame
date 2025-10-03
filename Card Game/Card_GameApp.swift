45573d8d1997182d6a769f31a958f31ca05685a3
Karl Legson <klegson48@gmail.com>
2025-10-02 18:38:08 -0700

    Initial commit: Card Game SwiftUI project with live preview setup

---
+++ Card Game/Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_GameApp.swift
+//  Card Game
+//
+//  Created by Karl on 10/2/25
+//
+
+import SwiftUI
+
+@main
+struct Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Card Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Card Game
+//
+//  Created by Karl on 10/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, wd!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
